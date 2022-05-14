6bc6c5bf974e77d5be08cdad59ecfbdf7fdaf542
erickills <38625756+erickills@users.noreply.github.com>
2022-05-14 19:11:08 +0800

    Initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `/erc/`,
+    title: `er.c`,
     author: {
       name: `Ericson Santiago`,
       summary: ` I'm using this to document my personal learnings, attempts, experiments with various technologies that fascinates me, We get better by trying.`,
